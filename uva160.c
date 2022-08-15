6f3c0c947906e16e4b4368e548f7d8255089216c
31461057 <htchung@gms.tku.edu.tw>
2022-08-15 14:29:57 +0800

    W08-P2: add w08 md link in README.md

---
+++ uva160.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+int y=0,i,n,j,a[100],t[100],m,m1,c=0,c1=0;
+while(y==0){
+	scanf("%d",&n);
+	if(n==0){
+		break;
+	}
+	for(i=0;i<n;i++){
+		a[i]=i+1;
+	}
+	for(i=0;i<=n;i++){
+		t[i]=0;
+	}
+	for(i=0;i<n;i++){
+		m=a[i];
+		for(j=2;j<=n;j++){
+		m1=m%j;
+		while(m1==0){
+			c++;
+			m=m/j;
+			m1=m%j;
+			
+			}
+		c1=t[j];
+		t[j]=c1+c;
+		c=0;	
+		}
+	}
+	for(j=2;j<=n;j++){
+		if(t[j]!=0){
+			c++;
+		if(c==16){
+		printf("\n");
+				}
+	printf("%d ",t[j]);
+
+			}
+		}
+	c=0;
+	printf("\n");
+	}
+}
+
+
